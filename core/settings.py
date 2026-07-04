863a513411dc7de32b10ff81a908b431efc06974
natandavinci <natanaelqueiroz969@gmail.com>
2026-07-04 17:33:18 -0300

    add: postgree

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+POSTGRE_PASSWORD = os.getenv("POSTGRE_PASSWORD")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nail_design_db',
+        'USER': 'postgres',
+        'PASSWORD': POSTGRE_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
